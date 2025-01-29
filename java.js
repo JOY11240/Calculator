49349bae9561276ec74f49842d8342984c7394c4
Dev Joy <codshop48@gmail.com>
2025-01-29 23:14:40 +0630

    upload

--- java.js
+++ java.js
@@ -1,6 +1,14 @@
 let btn = document.getElementById("btn");
 let result = document.getElementById("result");
-let finel = "";
+let num1Error = document.getElementById("num1Error");
+let num2Error = document.getElementById("num2Error");
+let optionError = document.getElementById("optionError");
+let num1Status , num2Status , optionStatus = true;
+let finel;
+
+num1Error.style.display = "none";
+num2Error.style.display = "none";
+optionError.style.display = "none";
 
 btn.addEventListener("click" , function () {
 
@@ -8,6 +16,9 @@ btn.addEventListener("click" , function () {
     let num2 = document.getElementById("num2");
     let option = document.getElementById("option");
 
+    checkIn(num1 , num2 , option);
+
+    if (num1Status && num2Status && optionStatus == true){
     num1 = parseFloat(num1.value);
     num2 = parseFloat(num2.value);
     
@@ -26,4 +37,31 @@ btn.addEventListener("click" , function () {
     }
 
     result.innerHTML = (finel);
+    }
 })
+
+    function checkIn (num1 , num2 , option) {
+    if (num1.value == "") {
+        num1Error.style.display = "block";
+        num1Status = false;
+    } else {
+        num1Error.style.display = "none";
+        num1Status = true;
+    }
+
+    if (num2.value == "") {
+        num2Error.style.display = "block";
+        num2Status = false;
+    } else {
+        num2Error.style.display = "none";
+        num2Status = true;
+    }
+
+    if (option.value == "empty") {
+        optionError.style.display = "block";
+        optionStatus = false;
+    } else {
+        optionError.style.display = "none";
+        optionStatus = true;
+    }
+}
